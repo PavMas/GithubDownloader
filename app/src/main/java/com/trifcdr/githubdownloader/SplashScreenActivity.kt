29f631ef60a252bc5741b1e29f489012d43e48e3
PavMas <pashamaslov12@gmail.com>
2022-11-23 00:51:34 +0300

    MaterialTheme + SplashScreen

--- app/src/main/java/com/trifcdr/githubdownloader/SplashScreenActivity.kt
+++ app/src/main/java/com/trifcdr/githubdownloader/SplashScreenActivity.kt
@@ -1,11 +1,25 @@
 package com.trifcdr.githubdownloader
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.trifcdr.githubdownloader.databinding.ActivitySplashScreenBinding
+import com.trifcdr.githubdownloader.presentation.MainActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        val binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val splash = binding.splash
+        splash.alpha = 0F
+        splash.animate().setDuration(1300).alpha(1F).withEndAction{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
     }
 }
